825700edb525b716e3f58e00103d12d7936767be
juanmanueldaza <juanmanueldaza@gmail.com>
2022-10-17 13:29:52 -0300

    Componentización interna de Componente Pokedex

---
+++ src/components/sub/Glows.vue
@@ -0,0 +1,102 @@
+<script></script>
+
+<template>
+  <g id="glows">
+    <g id="secondary_screen_glow">
+      <path
+        id="_317603240"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M266.2 594.5H252.5L191.1 654.4H206.7L266.2 594.5Z"
+        fill="#9AD8A2"
+      />
+      <path
+        id="_317603336"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M298.5 594.5H271.2L210.8 654.4H239.9L298.5 594.5Z"
+        fill="#92D59B"
+      />
+    </g>
+    <g id="small_screen_two_glow">
+      <path
+        id="_317599616"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M885.8 659.6H872.1L907.9 623.6H922.2L885.8 659.6Z"
+        fill="#424242"
+      />
+      <path
+        id="_317599472"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M915.1 659.6H895.8L931.3 623.6H951L915.1 659.6Z"
+        fill="#424242"
+      />
+    </g>
+    <g id="small_screen_one_glow">
+      <path
+        id="_317599688"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M679.3 659.6H672.8L708.6 623.6H715.8L679.3 659.6Z"
+        fill="#424242"
+      />
+      <path
+        id="_317598248"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M705.1 659.6H685.8L721.3 623.6H741L705.1 659.6Z"
+        fill="#424242"
+      />
+    </g>
+    <g id="tertiary_screen_glow">
+      <path
+        id="_317598728"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M863.1 228.9L849.3 228.8L692.1 386.2H706.3L863.1 228.9Z"
+        fill="#424242"
+      />
+      <path
+        id="_317598824"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M931.3 228.9L876.5 228.8L719.3 386.2H774.4L931.3 228.9Z"
+        fill="#424242"
+      />
+      <path
+        id="_317598512"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M975.7 228.9L947.4 228.8L790.1 386.2H818.8L975.7 228.9Z"
+        fill="#424242"
+      />
+    </g>
+    <g id="primary_screen_glow">
+      <path
+        id="_202166936"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M333.2 259.7H343.6L154 448.9H151.1C148.9 448.9 146.8 448.6 144.7 448.2L333.2 259.7Z"
+        fill="#F8FFFF"
+      />
+      <path
+        id="_202165760"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M268 259.7H310.1L129.3 438.6C125.3 433.7 122.9 427.5 122.9 420.8V404.1L268 259.7Z"
+        fill="#F8FFFF"
+      />
+      <path
+        id="_202172912"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M226.1 259.7L122.9 360.7V393.1L255.5 259.7H226.1V259.7Z"
+        fill="#E8FFFF"
+      />
+    </g>
+  </g>
+</template>
+
+<style scoped></style>
---
+++ src/components/sub/Screens.vue
@@ -0,0 +1,79 @@
+<script></script>
+
+<template>
+  <g id="screens">
+    <path
+      id="secondary_screen"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M149 594.5H305.8C309.7 594.5 312.8 597.7 312.8 601.5V647.3C312.8 651.2 309.6 654.3 305.8 654.3H149C145.1 654.3 142 651.1 142 647.3V601.5C142 597.7 145.1 594.5 149 594.5Z"
+      fill="#39B348"
+    />
+    <foreignObject x="140" y="595" height="60" width="175">
+      <div class="inputContainer">
+        <input
+          id="input"
+          type="text"
+          placeholder="pokémon"
+          autocomplete="off"
+        />
+      </div>
+    </foreignObject>
+    <path
+      id="tertiary_screen"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M672.5 228.8H976C1004 228.8 1012.8 238.3 1012.8 256.2V357.2C1012.8 375 1005 386.2 977.1 386.2H672.1C644.1 386.2 634.4 375.1 634.4 357.2V256.2C634.4 238.3 644.5 228.8 672.5 228.8Z"
+      fill="#2C2C2C"
+    />
+    <foreignObject x="640" y="230" width="365" height="150">
+      <div id="pokeNameContainer">
+        <h2 id="pokeName"></h2>
+      </div>
+    </foreignObject>
+    <path
+      id="small_screen_one"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M669.4 623.6H778.7C780.6 623.6 782.1 625.1 782.1 627V656.2C782.1 658.1 780.6 659.6 778.7 659.6H669.4C667.5 659.6 666 658.1 666 656.2V627C666.1 625.2 667.6 623.6 669.4 623.6Z"
+      fill="#2C2C2C"
+    />
+    <foreignObject x="663" y="621" width="125" height="40">
+      <div id="pokeGenderContainer">
+        <h3 id="pokeGender"></h3>
+      </div>
+    </foreignObject>
+    <path
+      id="small_screen_two"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M868.8 623.6H978.1C980 623.6 981.5 625.1 981.5 627V656.2C981.5 658.1 980 659.6 978.1 659.6H868.8C867.9 659.6 867 659.2 866.4 658.6C865.8 658 865.4 657.1 865.4 656.2V627C865.4 625.2 866.9 623.6 868.8 623.6Z"
+      fill="#2C2C2C"
+    />
+    <foreignObject x="861" y="621" width="125" height="40">
+      <div id="pokeTypesContainer">
+        <h3 id="pokeTypes"></h3>
+      </div>
+    </foreignObject>
+    <path
+      id="main_screen"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M151.1 259.7H373.8C389.3 259.7 402 272.4 402 287.9V420.8C402 436.3 389.3 449 373.8 449H151.1C135.6 449 122.9 436.3 122.9 420.8V287.8C122.9 272.4 135.6 259.7 151.1 259.7Z"
+      fill="#CCFFFE"
+    />
+    <foreignObject x="185" y="285" width="200" height="200">
+      <img
+        id="loadingGifAnimation"
+        width="70%"
+        src="assets\animation.gif"
+        alt=""
+      />
+    </foreignObject>
+    <foreignObject x="170" y="255" width="200" height="200">
+      <img id="pokeImg" src="" alt="" />
+    </foreignObject>
+  </g>
+</template>
+
+<style scoped></style>
